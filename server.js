110a7edff3b1484749f0216f2dfc6ec8584b223f
matematicaspoemas-cmd <matematicaspoemas@gmail.com>
2026-05-07 14:50:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,39 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hola estoy actualizando.js')
+  res.send(`
+    <html>
+      <head>
+        <title>Mi Primera App</title>
+      </head>
+
+      <body style="
+        font-family: Arial;
+        background: #111;
+        color: white;
+        text-align: center;
+        padding-top: 50px;
+      ">
+
+        <h1>🚀 Mi App Node.js</h1>
+
+        <p>Servidor funcionando correctamente</p>
+
+        <button onclick="mostrarMensaje()">
+          Haz clic aquí
+        </button>
+
+        <script>
+          function mostrarMensaje() {
+            alert('Hola Alex 🚀')
+          }
+        </script>
+
+      </body>
+    </html>
+  `)
 })
 
 app.listen(PORT, () => {
-  console.log(`Servidor funcionando en puerto ${PORT}`)
+  console.log(\`Servidor funcionando en puerto \${PORT}\`)
 })
