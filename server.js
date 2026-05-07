d5a45024f3485cd063c50fb888cd2db98b5a8ff5
AlexJimenez <matematicaspoemas@gmail.com>
2026-05-07 14:17:45 -0500

    remove emoji

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hola desde Node.js 🚀')
+  res.send('Hola desde Node.js')
 })
 
 app.listen(PORT, () => {
